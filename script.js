30a5ecb9424bc2320d2138ce5b3cf1b14d99d301
Rapha Sales <slraphadev@gmail.com>
2024-07-09 02:53:15 -0300

    final commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleMode() {
     html.classList.toggle("light");
     const img = document.querySelector("#profile img");
     if (html.classList.contains("light")) {
-    img.setAttribute('src', "assets/avatar-light.png");
+        img.setAttribute('src', "assets/avatar-light.png");
     } else {
         img.setAttribute('src', "assets/avatar.png");
     }
